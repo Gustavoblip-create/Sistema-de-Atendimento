aac349bead2c48b4177049be31a91df2a70b2fb4
Luis Gustavo <luisgustavomarilia52@gmail.com>
2025-08-19 21:11:31 -0300

    Desenvolvi a classe model Cliente

--- Model/Clientes.cs
+++ Model/Clientes.cs
@@ -8,5 +8,29 @@ namespace Sistema_de_Atendimento.Model
 {
     internal class Clientes
     {
-    }
+        public int id { get; set; }
+
+        public string nome { get; set; }
+
+        public string email { get; set; }
+
+        public string cpf_cnpj{ get; set; }
+        public string tipo_pessoa{ get; set; }
+
+        public string telefone { get; set; }
+        public string celular { get; set; }
+
+        public string cep { get; set; }
+        public string endereco { get; set; }
+
+        public string numero { get; set; }
+        public string complemento{ get; set; }
+        public string bairro{ get; set; }
+        public string cidade{ get; set; }
+        public string estado{ get; set; }
+        public int ativo{ get; set; }
+
+
+
+}
 }
